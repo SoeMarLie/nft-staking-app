9c0142c5c38f6833320fd6621588a04aa5c433d2
SoeMarLie <91035687+SoeMarLie@users.noreply.github.com>
2022-12-30 01:46:47 +0700

    Update stake.tsx

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -11,9 +11,9 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
-const nftDropContractAddress = "0x322067594DBCE69A9a9711BC393440aA5e3Aaca1";
-const tokenContractAddress = "0xb1cF059e6847e4270920a02e969CA2E016AeA22B";
-const stakingContractAddress = "0xB712975e13427ac804177E7CebF08781bbF9B89c";
+const nftDropContractAddress = "0xA0Bf8612B66791b0700C271417DE8583d9a6d5fb";
+const tokenContractAddress = "0x0368a408e585944bD4583E99E593cBEa250F3f3c";
+const stakingContractAddress = "0xb6e38F694daC77Ab9b80D7c7133372830f50e229";
 
 const Stake: NextPage = () => {
   // Wallet Connection Hooks
