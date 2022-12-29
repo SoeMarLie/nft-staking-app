1641c8c0cf770a95309926580257f9d5773cf896
SoeMarLie <91035687+SoeMarLie@users.noreply.github.com>
2022-12-29 18:40:50 +0000

    Pending changes exported from your codespace

---
+++ ethriseartnftstaking/pages/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>Welcome to IBW-NFT</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/mint`)}
+        >
+          {/* Mint a new NFT */}
+            <img src={`/icons/drop.webp`} alt="drop" />
+            <h2 className={styles.selectBoxTitle}>Mint a new IBW NFT</h2>
+            <p className={styles.selectBoxDescription}>
+              Just connect your wallet to claim an NFT from the collection.
+            </p>
+        </div>
+
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <img src={`/icons/token.webp`} alt="drop" />
+          <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
+          <p className={styles.selectBoxDescription}>
+          Stake the nft you have <b>Here</b>{" "}
+             to get icn <b>Token</b> as a reward.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
